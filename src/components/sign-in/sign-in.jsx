1b0fb80356e2c8828c7cc0da0ae6db8af3a9e873
iswarya <iswarya.ch99@gmail.com>
2023-06-14 18:59:19 +0530

    added form input

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,9 +1,63 @@
 import React, { Component } from "react";
 
 import "./sign-in.scss";
+import FormInput from "../form-input/form-input";
+import CustomButton from "../custom-button/custom-button";
 
 class SignIn extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  handleSubmit = (event) => {
+    console.log(event);
+    event.preventDefault();
+  };
+
+  handleChange = (event) => {
+    console.log("Event => ", event);
+    let { value, name } = event.target;
+    console.log(name);
+    this.setState({
+      [name]: value,
+    });
+  };
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <label>Email</label>
+          <FormInput
+            name="email"
+            type="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="email"
+          />
+
+          <label>Password</label>
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label="password"
+          />
+
+          <CustomButton type="submit" value="Submit Form">
+            Sign In
+          </CustomButton>
+        </form>
+      </div>
+    );
   }
 }
+
+export default SignIn;
--- src/pages/sign-in-and-sign-out/sign-in-and-sign-out.jsx
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 import "./sign-in-and-sign-out.scss";
+import SignIn from "../../components/sign-in/sign-in";
 
 const SignInAndSignOutPage = () => (
-  <div className="sign-in-and-sign-out">SIGN IN</div>
+  <div className="sign-in-and-sign-out">
+    <SignIn />
+  </div>
 );
 
 export default SignInAndSignOutPage;
